8b363857897f231f0e17ff8f5c703a09ec14d169
the_xu <448537509@qq.com>
2025-02-09 01:07:37 +0800

    移速提高

--- src/main/java/org/confluence/terraentity/entity/summon/SummonIronGolem.java
+++ src/main/java/org/confluence/terraentity/entity/summon/SummonIronGolem.java
@@ -4,6 +4,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
@@ -11,11 +12,13 @@ import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.IronGolem;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
+import org.confluence.terraentity.TerraEntity;
 import org.confluence.terraentity.entity.ai.goal.summon.SummonMeleeAttackGoal;
 
 import java.util.Optional;
@@ -23,8 +26,11 @@ import java.util.UUID;
 
 public class SummonIronGolem extends IronGolem implements ISummonMob<SummonIronGolem> {
 
+    static ResourceLocation moveSpeedModify = TerraEntity.space("summon");
     public SummonIronGolem(EntityType<? extends IronGolem> entityType, Level level) {
         super(entityType, level);
+        if(!this.getAttribute(Attributes.MOVEMENT_SPEED).hasModifier(moveSpeedModify))
+            this.getAttribute(Attributes.MOVEMENT_SPEED).addTransientModifier(new AttributeModifier(moveSpeedModify, 0.2, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
     }
 
     @Override
