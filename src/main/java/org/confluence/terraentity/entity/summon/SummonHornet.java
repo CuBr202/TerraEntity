b0bc3232cac42e6be0cf71bdcb95d7d4b9f68e3e
the_xu <448537509@qq.com>
2025-03-01 01:09:47 +0800

    修正ai

--- src/main/java/org/confluence/terraentity/entity/summon/SummonHornet.java
+++ src/main/java/org/confluence/terraentity/entity/summon/SummonHornet.java
@@ -121,7 +121,7 @@ public class SummonHornet extends Hornet implements ISummonMob<SummonHornet>{
     @Override
     public boolean canAttack(LivingEntity target) {
         if(target == summon_getOwner()) return false;
-        return super.canAttack(target);
+        return target.canBeSeenAsEnemy();
     }
 
     @Override
