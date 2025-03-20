d98429af86c8a25f0913587517f672c3a62dc222
westernat <westernat233@163.com>
2025-03-20 19:54:22 +0800

    将部分entityJoinLevel事件移到finalizeSpawn事件

--- src/main/java/org/confluence/terraentity/event/GameEntityEvent.java
+++ src/main/java/org/confluence/terraentity/event/GameEntityEvent.java
@@ -15,6 +15,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
@@ -24,7 +25,6 @@ import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import net.neoforged.neoforge.event.tick.EntityTickEvent;
 import org.confluence.terraentity.entity.ai.Boss;
 import org.confluence.terraentity.entity.monster.AbstractMonster;
 import org.confluence.terraentity.entity.monster.Decayeder;
@@ -46,39 +46,21 @@ import static org.confluence.terraentity.TerraEntity.MODID;
 public class GameEntityEvent {
     @SubscribeEvent
     public static void entityJoinLevel(EntityJoinLevelEvent event) {
-
-
-        Level level = event.getLevel();
-        if (event.loadedFromDisk() || !(level instanceof ServerLevel serverLevel)) return;
-        if (event.getEntity() instanceof Zombie zombie && !zombie.isBaby() && !zombie.isVehicle() && zombie.getRandom().nextFloat() < 0.05F) {
-            BaseSlime slime = TEEntities.BLUE_SLIME.get().create(level);
-            if (slime != null) {
-                slime.moveTo(zombie.getX(), zombie.getY(), zombie.getZ(), zombie.getYRot(), 0.0F);
-                slime.finalizeSpawn(serverLevel, level.getCurrentDifficultyAt(zombie.blockPosition()), MobSpawnType.JOCKEY, null);
-                slime.startRiding(zombie);
-                level.addFreshEntity(slime);
-            }
-        }
-        if(event.getEntity() instanceof Monster living && !(event.getEntity() instanceof ISummonMob<?>))
-            TEUtils.monsterEnhance(living);
-        else if(event.getEntity() instanceof Slime slime)
-            TEUtils.monsterEnhance(slime);
         // 生成信息
         Boss.sendBossSpawnMessage(event.getEntity());
     }
 
     @SubscribeEvent
-    public static void entityLeaveLevelEvent (EntityLeaveLevelEvent event) {
-        if(event.getEntity() instanceof ServerPlayer player){
+    public static void entityLeaveLevelEvent(EntityLeaveLevelEvent event) {
+        if (event.getEntity() instanceof ServerPlayer player) {
             // 清除召唤物
             player.getData(TEAttachments.SUMMONER_STORAGE.get()).clear(player);
         }
-
     }
 
     @SubscribeEvent
     public static void playerRespawn(PlayerEvent.PlayerRespawnEvent event) {
-        if(event.getEntity() instanceof ServerPlayer player) {
+        if (event.getEntity() instanceof ServerPlayer player) {
             // 同步召唤栏信息
             player.getData(TEAttachments.SUMMONER_STORAGE.get()).sync(player);
         }
@@ -88,7 +70,7 @@ public class GameEntityEvent {
     public static void entityDeathLevel(LivingDeathEvent event) {
         Level level = event.getEntity().level();
         Boss.sendBossDeathMessage(event.getEntity());
-        if(event.getEntity() instanceof ServerPlayer player){
+        if (event.getEntity() instanceof ServerPlayer player) {
             player.getData(TEAttachments.SUMMONER_STORAGE.get()).clear(player);
         }
     }
@@ -100,8 +82,8 @@ public class GameEntityEvent {
         LivingEntity e1 = event.getEntity();
         Level level = event.getEntity().level();
         if (!(level instanceof ServerLevel serverLevel)) return;
-        if (event.getSource().getEntity() instanceof Decayeder dp){
-            if (!e1.hasEffect(TEEffects.DEMONIC_THOUGHTS)){
+        if (event.getSource().getEntity() instanceof Decayeder dp) {
+            if (!e1.hasEffect(TEEffects.DEMONIC_THOUGHTS)) {
                 e1.addEffect(new MobEffectInstance(
                         TEEffects.DEMONIC_THOUGHTS, 200
                 ), dp);
@@ -109,7 +91,7 @@ public class GameEntityEvent {
                 e1.removeEffect(TEEffects.DEMONIC_THOUGHTS);
                 e1.hurt(event.getSource(), 6);
                 AbstractMonster soulEater = TEEntities.EATER_OF_SOULS.get().create(level);
-                if (soulEater!=null) {
+                if (soulEater != null) {
                     soulEater.setPos(e1.getEyePosition());
                     soulEater.setTarget(e1);
                     level.addFreshEntity(soulEater);
@@ -120,17 +102,17 @@ public class GameEntityEvent {
     }
 
     @SubscribeEvent
-    public static void entityInteract(PlayerInteractEvent.EntityInteract event){
+    public static void entityInteract(PlayerInteractEvent.EntityInteract event) {
         ItemStack item = event.getItemStack();
-        if(!(event.getTarget() instanceof LivingEntity entity)) return;
+        if (!(event.getTarget() instanceof LivingEntity entity)) return;
         Player player = event.getEntity();
         Level level = event.getLevel();
-        if (    entity.getType().equals(TEEntities.BLUE_SLIME.get()) ||
+        if (entity.getType().equals(TEEntities.BLUE_SLIME.get()) ||
                 entity.getType().equals(TEEntities.GREEN_SLIME.get()) ||
-                entity.getType().equals(TEEntities.PURPLE_SLIME.get())){
-            if (item.is(TETags.Items.HONEY_TRANSLATION_BUCKET)){
+                entity.getType().equals(TEEntities.PURPLE_SLIME.get())) {
+            if (item.is(TETags.Items.HONEY_TRANSLATION_BUCKET)) {
                 HoneySlime slime = TEEntities.HONEY_SLIME.get().create(level);
-                if (slime!=null) {
+                if (slime != null) {
                     item.shrink(1);
                     player.addItem(new ItemStack(Items.BUCKET));
                     slime.setSize(2, true);
@@ -140,9 +122,9 @@ public class GameEntityEvent {
                     level.addFreshEntity(slime);
                 }
                 entity.remove(Entity.RemovalReason.DISCARDED);
-            } else if (item.is(TETags.Items.HONEY_TRANSLATION)){
+            } else if (item.is(TETags.Items.HONEY_TRANSLATION)) {
                 HoneySlime slime = TEEntities.HONEY_SLIME.get().create(level);
-                if (slime!=null) {
+                if (slime != null) {
                     item.shrink(1);
                     slime.setSize(2, true);
                     slime.setPos(entity.position());
@@ -151,9 +133,9 @@ public class GameEntityEvent {
                     level.addFreshEntity(slime);
                 }
                 entity.remove(Entity.RemovalReason.DISCARDED);
-            } else if (item.is(TETags.Items.HONEY_TRANSLATION_NOT_CONSUMED)){
+            } else if (item.is(TETags.Items.HONEY_TRANSLATION_NOT_CONSUMED)) {
                 HoneySlime slime = TEEntities.HONEY_SLIME.get().create(level);
-                if(slime!=null) {
+                if (slime != null) {
                     slime.setSize(2, true);
                     slime.setPos(entity.position());
                     slime.setXRot(entity.getXRot());
@@ -177,12 +159,19 @@ public class GameEntityEvent {
         } else if (mob instanceof BlackSlime blackSlime) {
             blackSlime.finalizeSpawn(randomSource, event.getDifficulty());
         }
-
+        ServerLevelAccessor level = event.getLevel();
+        if (event.getEntity() instanceof Zombie zombie && !zombie.isBaby() && !zombie.isVehicle() && zombie.getRandom().nextFloat() < 0.05F) {
+            BaseSlime slime = TEEntities.BLUE_SLIME.get().create(mob.level());
+            if (slime != null) {
+                slime.moveTo(zombie.getX(), zombie.getY(), zombie.getZ(), zombie.getYRot(), 0.0F);
+                slime.finalizeSpawn(level, event.getDifficulty(), MobSpawnType.JOCKEY, null);
+                slime.startRiding(zombie);
+                level.addFreshEntity(slime);
+            }
+        }
+        if (event.getEntity() instanceof Monster living && !(event.getEntity() instanceof ISummonMob<?>))
+            TEUtils.monsterEnhance(living);
+        else if (event.getEntity() instanceof Slime slime)
+            TEUtils.monsterEnhance(slime);
     }
-
-    @SubscribeEvent
-    public static void tickEntity(EntityTickEvent.Post event) {
-
-    }
-
 }
