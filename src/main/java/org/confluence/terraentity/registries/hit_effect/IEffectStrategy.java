9dd504d10a88fd150644302a4eb3da160656524c
the_xu <448537509@qq.com>
2025-03-22 00:39:26 +0800

    补齐效果翻译和注释

--- src/main/java/org/confluence/terraentity/registries/hit_effect/IEffectStrategy.java
+++ src/main/java/org/confluence/terraentity/registries/hit_effect/IEffectStrategy.java
@@ -13,24 +13,23 @@ import java.util.List;
 import java.util.function.BiConsumer;
 
 /**
- * <h1>自定义效果</h1>
+ * <h1> 自定义效果 </h1>
+ * <p> 定义不同的编解码器，方便数据生成效果的参数
  */
 public interface IEffectStrategy {
 
     /**
-     * 应用效果
+     * 具体的应用效果
      */
     BiConsumer<LivingEntity, LivingEntity> getEffect();
 
-
+    /**
+     * 翻译键后缀
+     */
     String getName();
 
     default String getTranslationKey() {
-        try {
-            return TerraEntity.MODID + ".effect.strategy." + getName();
-        }catch (Exception e) {
-            return TerraEntity.MODID + ".effect.strategy.unknown";
-        }
+        return TerraEntity.MODID + ".effect.strategy." + getName();
 //        return "effect.strategy." + TERegistries.EffectStrategyProviders.REGISTRY.getKey(getCodec().get());
     }
 
@@ -42,6 +41,17 @@ public interface IEffectStrategy {
      * 效果描述
      */
     static void appendDescription(List<Component> tooltipComponents, List<? extends IEffectStrategy> effectStrategy, Component title) {
+        int size = effectStrategy.size();
+        if(size == 0) return;
+        tooltipComponents.add(title);
+        for(int i = 0; i < size; i++) {
+            IEffectStrategy effect = effectStrategy.get(i);
+            tooltipComponents.add(Component.literal(" - ").append(effect.getDescription()).withColor(0xFF00FF));
+        }
+    }
+
+    static void appendDescriptions(List<Component> tooltipComponents, List<EffectStrategyComponent> components, Component title) {
+        List<IEffectStrategy> effectStrategy = components.stream().flatMap(e->e.effects().stream()).toList();
         int size = effectStrategy.size();
         if(size == 0) return;
         tooltipComponents.add(title);
