938052dc189e1e8494c2c3c396f263a26c83be25
the_xu <448537509@qq.com>
2025-03-23 23:48:04 +0800

    改弹幕发射速率

--- src/main/java/org/confluence/terraentity/entity/summon/SummonHornet.java
+++ src/main/java/org/confluence/terraentity/entity/summon/SummonHornet.java
@@ -34,7 +34,7 @@ public class SummonHornet extends Hornet implements ISummonMob<SummonHornet>{
     @Override
     protected void registerGoals() {
 //        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 2, true));
-        this.goalSelector.addGoal(1, new BeeShootGoal(this, 0, 5){
+        this.goalSelector.addGoal(1, new BeeShootGoal(this, 0, 10){
             @Override
             protected boolean canShoot(Entity target) {
                 return true;
