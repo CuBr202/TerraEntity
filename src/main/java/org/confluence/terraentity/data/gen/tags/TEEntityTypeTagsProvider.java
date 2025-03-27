4c6799906a0a990e9ebea3534d27781ffd70912e
westernat <westernat233@163.com>
2025-03-27 16:57:24 +0800

    禁止新生魔艺的收容罐抓波斯

--- src/main/java/org/confluence/terraentity/data/gen/tags/TEEntityTypeTagsProvider.java
+++ src/main/java/org/confluence/terraentity/data/gen/tags/TEEntityTypeTagsProvider.java
@@ -1,9 +1,12 @@
 package org.confluence.terraentity.data.gen.tags;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -24,34 +27,39 @@ public class TEEntityTypeTagsProvider extends EntityTypeTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         Stream.of(TETags.EntityTypes.SLIME, EntityTypeTags.NON_CONTROLLING_RIDER)
-            .forEach(type->tag(type).add(
-                TEEntities.BLUE_SLIME.get(),
-                TEEntities.GREEN_SLIME.get(),
-                TEEntities.PINK_SLIME.get(),
-                TEEntities.CORRUPTED_SLIME.get(),
-                TEEntities.DESERT_SLIME.get(),
-                TEEntities.JUNGLE_SLIME.get(),
-                TEEntities.EVIL_SLIME.get(),
-                TEEntities.ICE_SLIME.get(),
-                TEEntities.LAVA_SLIME.get(),
-                TEEntities.LUMINOUS_SLIME.get(),
-                TEEntities.CRIMSON_SLIME.get(),
-                TEEntities.PURPLE_SLIME.get(),
-                TEEntities.RED_SLIME.get(),
-                TEEntities.TROPIC_SLIME.get(),
-                TEEntities.YELLOW_SLIME.get(),
-                TEEntities.HONEY_SLIME.get(),
-                TEEntities.BLACK_SLIME.get(),
-                EntityType.SLIME)
-            );
+                .forEach(type -> tag(type).add(
+                        TEEntities.BLUE_SLIME.get(),
+                        TEEntities.GREEN_SLIME.get(),
+                        TEEntities.PINK_SLIME.get(),
+                        TEEntities.CORRUPTED_SLIME.get(),
+                        TEEntities.DESERT_SLIME.get(),
+                        TEEntities.JUNGLE_SLIME.get(),
+                        TEEntities.EVIL_SLIME.get(),
+                        TEEntities.ICE_SLIME.get(),
+                        TEEntities.LAVA_SLIME.get(),
+                        TEEntities.LUMINOUS_SLIME.get(),
+                        TEEntities.CRIMSON_SLIME.get(),
+                        TEEntities.PURPLE_SLIME.get(),
+                        TEEntities.RED_SLIME.get(),
+                        TEEntities.TROPIC_SLIME.get(),
+                        TEEntities.YELLOW_SLIME.get(),
+                        TEEntities.HONEY_SLIME.get(),
+                        TEEntities.BLACK_SLIME.get(),
+                        EntityType.SLIME)
+                );
 
-        tag(Tags.EntityTypes.BOSSES)
-                .add(TEEntities.EYE_OF_CTHULHU.get())
-                .add(TEEntities.KING_SLIME.get())
-                .add(TEEntities.EATER_OF_WORLDS.get())
-                .add(TEEntities.EATER_OF_WORLD_SEGMENT.get())
-                .add(TEEntities.BRAIN_OF_CTHULHU.get());
-
+        EntityType<?>[] bosses = {
+                TEEntities.EYE_OF_CTHULHU.get(),
+                TEEntities.KING_SLIME.get(),
+                TEEntities.EATER_OF_WORLDS.get(),
+                TEEntities.EATER_OF_WORLD_SEGMENT.get(),
+                TEEntities.BRAIN_OF_CTHULHU.get(),
+                TEEntities.QUEEN_BEE.get(),
+                TEEntities.SKELETRON.get(),
+                TEEntities.SKELETRON_HAND.get()
+        };
+        tag(Tags.EntityTypes.BOSSES).add(bosses);
+        tag(TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.fromNamespaceAndPath("ars_nouveau", "jar_blacklist"))).add(bosses);
     }
 
 }
