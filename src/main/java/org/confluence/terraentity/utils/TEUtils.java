6a47a7106604457ee89e16a01c678c3d41a91019
the_xu <448537509@qq.com>
2025-03-29 14:25:50 +0800

    修复不能修改九头蛇属性

--- src/main/java/org/confluence/terraentity/utils/TEUtils.java
+++ src/main/java/org/confluence/terraentity/utils/TEUtils.java
@@ -236,16 +236,22 @@ public final class TEUtils {
             float multiplier = getMultiple(entity.level(), Attributes.MAX_HEALTH);
             if (dirty) {
                 int size = Math.min(entity.level().players().size(), 8);
-                if (!entity.getAttribute(Attributes.MAX_HEALTH).hasModifier(difficultyHealthKey))
-                    entity.getAttribute(Attributes.MAX_HEALTH).addPermanentModifier(new AttributeModifier(difficultyHealthKey, multiplier * size - 1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
-                if (!entity.getAttribute(Attributes.MAX_HEALTH).hasModifier(healthKey))
-                    entity.getAttribute(Attributes.MAX_HEALTH).addPermanentModifier(new AttributeModifier(healthKey, ServerConfig.BOSS_ATTRIBUTES_MULTIPLIER_HEALTH.get() - 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
-                entity.setHealth(entity.getMaxHealth());
+                var healthAttribute = entity.getAttribute(Attributes.MAX_HEALTH);
+                if (healthAttribute != null) {
+                    if (!healthAttribute.hasModifier(difficultyHealthKey))
+                        healthAttribute.addPermanentModifier(new AttributeModifier(difficultyHealthKey, multiplier * size - 1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
+                    if (!healthAttribute.hasModifier(healthKey))
+                        healthAttribute.addPermanentModifier(new AttributeModifier(healthKey, ServerConfig.BOSS_ATTRIBUTES_MULTIPLIER_HEALTH.get() - 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+                    entity.setHealth(entity.getMaxHealth());
+                }
             }
-            if(!entity.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(difficultyDamageKey))
-                entity.getAttribute(Attributes.ATTACK_DAMAGE).addTransientModifier(new AttributeModifier(difficultyDamageKey, multiplier - 1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
-            if(!entity.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(damageKey))
-                entity.getAttribute(Attributes.ATTACK_DAMAGE).addTransientModifier(new AttributeModifier(damageKey, ServerConfig.BOSS_ATTRIBUTES_MULTIPLIER_DAMAGE.get() - 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+            var damageAttribute = entity.getAttribute(Attributes.ATTACK_DAMAGE);
+            if (damageAttribute != null) {
+                if (!damageAttribute.hasModifier(difficultyDamageKey))
+                    damageAttribute.addTransientModifier(new AttributeModifier(difficultyDamageKey, multiplier - 1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
+                if (!damageAttribute.hasModifier(damageKey))
+                    damageAttribute.addTransientModifier(new AttributeModifier(damageKey, ServerConfig.BOSS_ATTRIBUTES_MULTIPLIER_DAMAGE.get() - 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+            }
         }
     }
 
@@ -254,14 +260,20 @@ public final class TEUtils {
         if(!ServerConfig.ENHANCE_ALL_MONSTER.get() && !BuiltInRegistries.ENTITY_TYPE.getKey(entity.getType()).getNamespace().equals(TerraEntity.MODID)) return;
         if(!entity.level().isClientSide) {
             float multiplier = getMultiple(entity.level(), Attributes.MAX_HEALTH);
-            if(!entity.getAttribute(Attributes.MAX_HEALTH).hasModifier(healthKey)){
-                entity.getAttribute(Attributes.MAX_HEALTH).addPermanentModifier(new AttributeModifier(healthKey, ServerConfig.MONSTER_ATTRIBUTES_MULTIPLIER_HEALTH.get() - 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
-                entity.setHealth(entity.getMaxHealth());
+            var healthAttribute = entity.getAttribute(Attributes.MAX_HEALTH);
+            if (healthAttribute != null) {
+                if (!healthAttribute.hasModifier(healthKey)) {
+                    healthAttribute.addPermanentModifier(new AttributeModifier(healthKey, ServerConfig.MONSTER_ATTRIBUTES_MULTIPLIER_HEALTH.get() - 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+                    entity.setHealth(entity.getMaxHealth());
+                }
             }
-            if(!entity.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(damageKey))
-                entity.getAttribute(Attributes.ATTACK_DAMAGE).addTransientModifier(new AttributeModifier(damageKey, ServerConfig.MONSTER_ATTRIBUTES_MULTIPLIER_DAMAGE.get() - 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
-            if(!entity.getAttribute(Attributes.ATTACK_DAMAGE).hasModifier(difficultyDamageKey))
-                entity.getAttribute(Attributes.ATTACK_DAMAGE).addTransientModifier(new AttributeModifier(difficultyDamageKey, multiplier - 1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
+            var damageAttribute = entity.getAttribute(Attributes.ATTACK_DAMAGE);
+            if (damageAttribute != null) {
+                if (!damageAttribute.hasModifier(damageKey))
+                    damageAttribute.addTransientModifier(new AttributeModifier(damageKey, ServerConfig.MONSTER_ATTRIBUTES_MULTIPLIER_DAMAGE.get() - 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+                if (!damageAttribute.hasModifier(difficultyDamageKey))
+                    damageAttribute.addTransientModifier(new AttributeModifier(difficultyDamageKey, multiplier - 1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
+            }
         }
     }
 
