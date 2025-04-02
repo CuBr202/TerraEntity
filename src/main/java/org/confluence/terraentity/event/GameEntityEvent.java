2046fc3fb02133aa41b996a627313f5cd3ad80a8
the_xu <448537509@qq.com>
2025-04-02 22:55:33 +0800

    修复刷怪笼史莱姆会卡

--- src/main/java/org/confluence/terraentity/event/GameEntityEvent.java
+++ src/main/java/org/confluence/terraentity/event/GameEntityEvent.java
@@ -209,7 +209,7 @@ public class GameEntityEvent {
                 slime.moveTo(zombie.getX(), zombie.getY(), zombie.getZ(), zombie.getYRot(), 0.0F);
                 slime.finalizeSpawn(level, event.getDifficulty(), MobSpawnType.JOCKEY, null);
                 slime.startRiding(zombie);
-                level.addFreshEntity(slime);
+//                level.addFreshEntity(slime);
             }
         }
         if (event.getEntity() instanceof Monster living && !(event.getEntity() instanceof ISummonMob<?>))
