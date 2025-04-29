ac25cb9eb4c7fe9b1c3e0730a2024fc02fffdb67
MagicHarp <@2749380016@qq.com>
2025-04-29 22:10:43 +0800

    规范一下翻译键

--- src/main/java/org/confluence/terraentity/data/gen/NPCMoodProvider.java
+++ src/main/java/org/confluence/terraentity/data/gen/NPCMoodProvider.java
@@ -25,56 +25,56 @@ public class NPCMoodProvider extends AbstractExistCodecProvider<Map<EntityType<?
         Map<EntityType<?>, NPCMoods.EntityMood> map = new HashMap<>();
         // goblin_tinkerer
         map.put(TENpcEntities.GOBLIN_TINKERER.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("goblin1"), MoodInfo.of(TENpcEntities.DYE_TRADER.get(), "goblin_tinkerer_like_dye_trader", Mood.LIKE))
+                .addMoodInfo(TerraEntity.space("goblin1"), MoodInfo.of(TENpcEntities.DYE_TRADER.get(), "mood.terra_entity.goblin_tinkerer.like.dye_trader", Mood.LIKE))
                 .build());
 
         // guide
         map.put(TENpcEntities.GUIDE.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("guild1"), MoodInfo.of(TENpcEntities.PAINTER.get(), "guide_hate_painter", Mood.HATE))
+                .addMoodInfo(TerraEntity.space("guild1"), MoodInfo.of(TENpcEntities.PAINTER.get(), "mood.terra_entity.guide.hate.painter", Mood.HATE))
                 .build());
 
         // arms_dealer
         map.put(TENpcEntities.ARMS_DEALER.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("arms_dealer1"), MoodInfo.of(TENpcEntities.DEMOLITIONIST.get(), "arms_dealer_hate_demolitionist", Mood.HATE))
-                .addMoodInfo(TerraEntity.space("arms_dealer2"), MoodInfo.of(TENpcEntities.NURSE.get(), "arms_dealer_love_nurse", Mood.LOVER))
+                .addMoodInfo(TerraEntity.space("arms_dealer1"), MoodInfo.of(TENpcEntities.DEMOLITIONIST.get(), "mood.terra_entity.arms_dealer.hate.demolitionist", Mood.HATE))
+                .addMoodInfo(TerraEntity.space("arms_dealer2"), MoodInfo.of(TENpcEntities.NURSE.get(), "mood.terra_entity.arms_dealer.love.nurse", Mood.LOVER))
                 .build());
 
         // angler
         map.put(TENpcEntities.ANGLER.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("angler1"), MoodInfo.of(TENpcEntities.DEMOLITIONIST.get(), "angler_like_demolitionist", Mood.LIKE))
+                .addMoodInfo(TerraEntity.space("angler1"), MoodInfo.of(TENpcEntities.DEMOLITIONIST.get(), "mood.terra_entity.angler.like.demolitionist", Mood.LIKE))
                 .build());
 
         // dye_trader
         map.put(TENpcEntities.DYE_TRADER.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("dye_trader1"), MoodInfo.of(TENpcEntities.ARMS_DEALER.get(), "dye_trader_like_arms_dealer", Mood.LIKE))
-                .addMoodInfo(TerraEntity.space("dye_trader2"), MoodInfo.of(TENpcEntities.PAINTER.get(), "dye_trader_like_painter", Mood.LIKE))
+                .addMoodInfo(TerraEntity.space("dye_trader1"), MoodInfo.of(TENpcEntities.ARMS_DEALER.get(), "mood.terra_entity.dye_trader.like.arms_dealer", Mood.LIKE))
+                .addMoodInfo(TerraEntity.space("dye_trader2"), MoodInfo.of(TENpcEntities.PAINTER.get(), "mood.terra_entity.dye_trader.like.painter", Mood.LIKE))
                 .build());
 
         // demolitionist
         map.put(TENpcEntities.DEMOLITIONIST.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("demolitionist1"), MoodInfo.of(TENpcEntities.ARMS_DEALER.get(), "demolitionist_dislike_arms_dealer", Mood.DISLIKE))
-                .addMoodInfo(TerraEntity.space("demolitionist2"), MoodInfo.of(TENpcEntities.GOBLIN_TINKERER.get(), "demolitionist_dislike_goblin_tinkerer", Mood.DISLIKE))
+                .addMoodInfo(TerraEntity.space("demolitionist1"), MoodInfo.of(TENpcEntities.ARMS_DEALER.get(), "mood.terra_entity.demolitionist.dislike.arms_dealer", Mood.DISLIKE))
+                .addMoodInfo(TerraEntity.space("demolitionist2"), MoodInfo.of(TENpcEntities.GOBLIN_TINKERER.get(), "mood.terra_entity.demolitionist.dislike.goblin_tinkerer", Mood.DISLIKE))
                 .build());
 
         // painter
         map.put(TENpcEntities.PAINTER.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("painter1"), MoodInfo.of(TENpcEntities.DRYAD.get(), "painter_love_dryad", Mood.LOVER))
+                .addMoodInfo(TerraEntity.space("painter1"), MoodInfo.of(TENpcEntities.DRYAD.get(), "mood.terra_entity.painter.love.dryad", Mood.LOVER))
                 .build());
 
         // dryad
         map.put(TENpcEntities.DRYAD.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("dryad1"), MoodInfo.of(TENpcEntities.ANGLER.get(), "dryad_dislike_angler", Mood.DISLIKE))
+                .addMoodInfo(TerraEntity.space("dryad1"), MoodInfo.of(TENpcEntities.ANGLER.get(), "mood.terra_entity.dryad.dislike.angler", Mood.DISLIKE))
                 .build());
 
         // merchant
         map.put(TENpcEntities.MERCHANT.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("merchant1"), MoodInfo.of(TENpcEntities.NURSE.get(), "merchant_like_nurse", Mood.LIKE))
+                .addMoodInfo(TerraEntity.space("merchant1"), MoodInfo.of(TENpcEntities.NURSE.get(), "mood.terra_entity.merchant.like.nurse", Mood.LIKE))
                 .build());
 
         // nurse
         map.put(TENpcEntities.NURSE.get(), new NPCMoods.EntityMood.Builder()
-                .addMoodInfo(TerraEntity.space("nurse1"), MoodInfo.of(TENpcEntities.ARMS_DEALER.get(), "nurse_love_arms_dealer", Mood.LOVER))
-                .addMoodInfo(TerraEntity.space("nurse2"), MoodInfo.of(TENpcEntities.DRYAD.get(), "nurse_dislike_dryad", Mood.DISLIKE))
+                .addMoodInfo(TerraEntity.space("nurse1"), MoodInfo.of(TENpcEntities.ARMS_DEALER.get(), "mood.terra_entity.nurse.love.rms_dealer", Mood.LOVER))
+                .addMoodInfo(TerraEntity.space("nurse2"), MoodInfo.of(TENpcEntities.DRYAD.get(), "mood.terra_entity.nurse.dislike.dryad", Mood.DISLIKE))
                 .build());
 
         gen(TerraEntity.space(NPCMoods.FILE_NAME), map);
--- src/main/java/org/confluence/terraentity/data/gen/NPCNameProvider.java
+++ src/main/java/org/confluence/terraentity/data/gen/NPCNameProvider.java
@@ -22,14 +22,134 @@ public class NPCNameProvider extends AbstractExistCodecProvider<Map<ResourceLoca
         gen(TerraEntity.space(NPCNames.FILE_NAME),Map.of(
                 TENpcEntities.GUIDE.getId(),
                 NPCNames.of(Map.of(
-                        "jack",5f,
-                        "daniel",10f
+                        "Andrew", 1f,
+                        "Asher", 1f,
+                        "Bradley", 1f,
+                        "Brandon", 1f,
+                        "Brett", 1f,
+                        "Brian", 1f,
+                        "Cody", 1f,
+                        "Cole", 1f,
+                        "Colin", 1f,
+                        "Connor", 1f
                 )),
                 TENpcEntities.NURSE.getId(),
                 NPCNames.of(Map.of(
-                        "jane",2f,
-                        "miss",3f)
-                )
+                        "Abigail", 1f,
+                "Allison", 1f,
+                "Amy", 1f,
+                "Caitlin", 1f,
+                "Carly", 1f,
+                "Claire", 1f,
+                "Emily", 1f,
+                "Emma", 1f,
+                "Hannah", 1f,
+                "Heather", 1f
+                )),
+                TENpcEntities.PAINTER.getId(),
+                NPCNames.of(Map.of(
+                "Bruno", 1f,
+                "Carlo", 1f,
+                "Darren", 1f,
+                "Enzo", 1f,
+                "Esreadel", 1f,
+                "Guido", 1f,
+                "Jim", 1f,
+                "Leonardo", 1f,
+                "Lorenzo", 1f,
+                "Luca", 1f
+                )),
+                TENpcEntities.MERCHANT.getId(),
+                NPCNames.of(Map.of(
+                "Alfred", 1f,
+                "Barney", 1f,
+                "Calvin", 1f,
+                "Edmund", 1f,
+                "Edwin", 1f,
+                "Eugene", 1f,
+                "Frank", 1f,
+                "Frederick", 1f,
+                "Gilbert", 1f,
+                "Gus", 1f
+                )),
+                TENpcEntities.GOBLIN_TINKERER.getId(),
+                NPCNames.of(Map.of(
+                "Arback", 1f,
+                "Dalek", 1f,
+                "Darz", 1f,
+                "Durnok", 1f,
+                "Fahd", 1f,
+                "Fjell", 1f,
+                "Gnudar", 1f,
+                "Grodax", 1f,
+                "Knogs", 1f,
+                "Knub", 1f
+                )),
+                TENpcEntities.DEMOLITIONIST.getId(),
+                NPCNames.of(Map.of(
+                "Bazdin", 1f,
+                "Beldin", 1f,
+                "Boften", 1f,
+                "Darur", 1f,
+                "Dias", 1f,
+                "Dolbere", 1f,
+                "Dolgen", 1f,
+                "Dolgrim", 1f,
+                "Duerthen", 1f,
+                "Durim", 1f
+                )),
+                TENpcEntities.DRYAD.getId(),
+                NPCNames.of(Map.of(
+                "Alalia", 1f,
+                "Alura", 1f,
+                "Ariella", 1f,
+                "Caelia", 1f,
+                "Calista", 1f,
+                "Celestia", 1f,
+                "Chryseis", 1f,
+                "Elysia", 1f,
+                "Emerenta", 1f,
+                "Evvie", 1f
+                )),
+                TENpcEntities.DYE_TRADER.getId(),
+                NPCNames.of(Map.of(
+                "Abdosir", 1f,
+                "Ahinadab", 1f,
+                "Ahirom", 1f,
+                "Akbar", 1f,
+                "Batnoam", 1f,
+                "Bodashtart", 1f,
+                "Danel", 1f,
+                "Hannibal", 1f,
+                "Hanno", 1f,
+                "Hiram", 1f
+                )),
+                TENpcEntities.ANGLER.getId(),
+                NPCNames.of(Map.of(
+                "Adam", 1f,
+                "Bart", 1f,
+                "Billy", 1f,
+                "Bobby", 1f,
+                "Charles", 1f,
+                "Danny", 1f,
+                "Grayson", 1f,
+                "Ivan", 1f,
+                "Izzy", 1f,
+                "Jey", 1f
+                )),
+                TENpcEntities.ARMS_DEALER.getId(),
+                NPCNames.of(Map.of(
+                "Andre", 1f,
+                "Brimst", 1f,
+                "Bronson", 1f,
+                "Dante", 1f,
+                "Darius", 1f,
+                "Darnell", 1f,
+                "Darryl", 1f,
+                "DeAndre", 1f,
+                "DeShawn", 1f,
+                "Demetrius", 1f
+                ))
         ));
     }
 
