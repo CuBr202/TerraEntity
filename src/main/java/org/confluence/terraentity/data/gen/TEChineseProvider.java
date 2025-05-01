0b818d18ab03017bb48f83be20f578d2ccbf4190
MagicHarp <161911368+MagicHarp@users.noreply.github.com>
2025-05-01 22:22:31 +0800

    琐碎的东西

--- src/main/java/org/confluence/terraentity/data/gen/TEChineseProvider.java
+++ src/main/java/org/confluence/terraentity/data/gen/TEChineseProvider.java
@@ -159,6 +159,7 @@ public class TEChineseProvider extends LanguageProvider {
         add(TESpawnEggItems.BIG_MUSCLE_ANGER_BONES_SPAWN_EGG.get(), "大肌肉愤怒骷髅刷怪蛋");
         add(TESpawnEggItems.BIG_HELMET_ANGER_BONES_SPAWN_EGG.get(), "大头盔愤怒骷髅刷怪蛋");
         add(TESpawnEggItems.CURSED_SKULL_SPAWN_EGG.get(), "诅咒骷髅头刷怪蛋");
+        add(TESpawnEggItems.DARK_CASTER_SPAWN_EGG.get(), "暗黑法师刷怪蛋");
 
         // boss
         add(TESpawnEggItems.KING_SLIME_SPAWN_EGG.get(), "史莱姆王刷怪蛋");
